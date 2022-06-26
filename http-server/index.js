cacf028c84e2950340a7a624e2c7826cd8e58f04
franciscobtd <franciscobernabe@drata.com>
2022-06-26 16:30:37 -0500

    feat: allow post request to add a new friend

--- http-server/index.js
+++ http-server/index.js
@@ -2,17 +2,41 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/friends") {
+const server = http.createServer();
+const friends = [
+  {
+    id: 0,
+    name: "Nicola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einsten",
+  },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString("utf-8");
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === "GET" && items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: "Sir Isaac Newton",
-      })
-    );
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const friend = friends.find((friend) => friend.id === Number(items[2]));
+      res.end(JSON.stringify(friend));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === "GET" && items[1] === "messages") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -23,6 +47,7 @@ const server = http.createServer((req, res) => {
     res.write("</ul>");
     res.write("</body>");
     res.write("</html>");
+    res.end();
   }
 });
 
