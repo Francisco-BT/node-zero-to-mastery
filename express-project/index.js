be0b8ca531dd6a805b8237fa32fd6efea44dce49
franciscobtd <franciscobernabe@drata.com>
2022-06-27 00:10:45 -0500

    feat: add endpoint to create a new friend and middleware to log actions

--- express-project/index.js
+++ express-project/index.js
@@ -14,6 +14,28 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} - ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      message: "Missing friend name",
+    });
+  }
+
+  const newFriend = { name: req.body.name, id: friends.length + 1 };
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
+
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
