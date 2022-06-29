4e201deaadabf4dd3eeb464dd0d35245b1c0b5b8
franciscobtd <franciscobernabe@drata.com>
2022-06-29 00:30:28 -0500

    feat: add support for static files

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  );
 }
 
 function postMessage(req, res) {
--- express-project/index.js
+++ express-project/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
@@ -11,6 +12,7 @@ app.use((req, res, next) => {
   const delta = Date.now() - start;
   console.log(`${req.method} - ${req.baseUrl}${req.url} ${delta}ms`);
 });
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
