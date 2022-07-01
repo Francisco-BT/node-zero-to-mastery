97f26494c3eb68064029d5cc667ad39d696b388d
franciscobtd <franciscobernabe@drata.com>
2022-06-30 23:53:22 -0500

    feat: add scripts for deploy

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
