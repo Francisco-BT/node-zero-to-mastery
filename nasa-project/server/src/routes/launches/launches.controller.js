38737b02b6bec48108d8948a572762f3b009064a
franciscobtd <franciscobernabe@drata.com>
2022-07-04 15:27:26 -0500

    feat: add validations to create a new launch

--- nasa-project/server/src/routes/launches/launches.controller.js
+++ nasa-project/server/src/routes/launches/launches.controller.js
@@ -6,10 +6,27 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunche(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Mising require launch property",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      message: "Invalid launch date",
+    });
+  }
 
   addNewLaunch(launch);
-  return res.sendStatus(201);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
