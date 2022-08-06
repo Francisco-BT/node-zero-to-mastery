9aafa1abc2d3382514229b3384967c9253604799
franciscobtd <franciscobernabe@drata.com>
2022-08-06 01:28:33 -0500

    feat: add google oauth flow git cookies

--- security-example/server.js
+++ security-example/server.js
@@ -2,20 +2,91 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const { Strategy } = require("passport-google-oauth20");
+
+require("dotenv").config();
 
 const PORT = 3000;
 const app = express();
 
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
+
+const AUTH_OPTIONS = {
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("google profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 1000 * 60 * 60 * 24,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/secret", (req, res) => {
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+  }),
+  (req, res) => {
+    console.log("Google call us back");
+  }
+);
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret is 42!");
 });
 
-http.createServer().listen(PORT, () => {
+app.get("/failure", (req, res) => res.send("Failed to log in!"));
+
+http.createServer(app).listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
