1a02146b267200bdd890f0e02f1d39ba6567268f
Francisco-BT <pancho10810@gmail.com>
2022-08-07 18:31:53 -0500

    feat: add socket connection between client and server

---
+++ multiplayer-pong/server.js
@@ -0,0 +1,17 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
