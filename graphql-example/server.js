72c99222a6afffae0098a8f51d573cacff011166
Francisco-BT <pancho10810@gmail.com>
2022-08-07 12:32:30 -0500

    refactor: modularize resolvers in its owns files

--- graphql-example/server.js
+++ graphql-example/server.js
@@ -6,21 +6,18 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
+  resolvers: resolversArray,
 });
-
-const root = {
-  products: require("./products/products.model"),
-  orders: require("./orders/orders.model"),
-};
 
 const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    rootValue: root,
     graphiql: true,
   })
 );
